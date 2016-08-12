d53929dcd46772cfd36d8d26efb9dbff91808553
Slime_hatena <slime.hatena@gmail.com>
2016-08-13 02:32:28 +0900

    Twitter関連の処理を追加

---
+++ testPost.php
@@ -0,0 +1,13 @@
+<?php
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/fcMgt4slStage/twitterLoader.php");
+
+//（＞▽＜）
+
+$tweet = "@tos （ ＞▽＜）＜" . date('Y年m月d日 H時i分s秒') . "だよ！ phpの接続テスト！";
+
+    // ツイート
+    $status = $to->post('statuses/update', ['status' => $tweet]);
+
+echo "<pre>";
+    var_dump($status);
+echo "</pre>";
---
+++ twitterLoader.php
@@ -0,0 +1,62 @@
+<?php
+// this file is include only!
+// twitter関連処理 ユーザーロード用
+/*
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/fcMgt4slStage/twitterLoader.php");
+------------------------------------------------------------------------------------------------
+$consumer_key
+$consumer_secret
+$oauth_token
+$oauth_token_secret
+$isLogin
+*/
+
+require_once dirname ( __FILE__ ) . "/lib/TwistOAuth.phar";
+require_once dirname ( __FILE__ ) . "/../../undefined/fcMgt4slStage.php";
+require_once dirname ( __FILE__ ) . "/../../undefined/DSN.php";
+session_start();
+
+$isLogin = false;
+
+try {
+    $pdo = new PDO ( 'mysql:host=' . $dsn ['host'] . ';dbname=' . $dsn ['dbname'] . ';charset=utf8', $dsn ['user'], $dsn ['pass'], array (
+    PDO::ATTR_EMULATE_PREPARES => false
+    ) );
+} catch ( PDOException $e ) {
+    $logWrite ="unSuccessful データベースの接続中にエラーが発生しました (userLoader)
+    sqlServer : connection unsuccess" . $e->getMessage ();
+    include($_SERVER['DOCUMENT_ROOT'] . "/fcMgt4slStage/log/logWriter.php");
+    exit ("ユーザーデータを取得中にエラーが発生しました。管理者にお問い合わせください。");
+}
+
+//セッションがあるか確認してなければクッキーで認証する
+  if (isset($_SESSION['_fcMgt4slStage'])){
+      $sql = "SELECT * FROM  `svrToolUser` WHERE  `session` = ?";
+      $key = $_SESSION['_fcMgt4slStage'];
+  }elseif (isset($_COOKIE['_fcMgt4slStage'])){
+    $sql = "SELECT * FROM  `svrToolUser` WHERE  `cookie` = ?";
+    $key = $_COOKIE['_fcMgt4slStage'];
+  }
+
+
+$stmt=$pdo->prepare($sql);
+$res=$stmt->execute(
+array($key));
+if ($res) {
+     foreach( $stmt as $p )
+    {
+        $query = $p ;
+    }
+$oauth_token = $query['oauth_token'];
+$oauth_token_secret = $query['oauth_token_secret'];
+$isLogin = true;
+}
+
+try {
+    $to = new TwistOAuth($consumer_key, $consumer_secret, $oauth_token, $oauth_token_secret);
+} catch(TwistException $e) {
+    $logWrite ="unSuccessful oauth認証に失敗しました (userLoader)
+    sqlServer : connection unsuccess" . $e->getMessage ();
+    include($_SERVER['DOCUMENT_ROOT'] . "/fcMgt4slStage/log/logWriter.php");
+    exit ("ユーザーデータを取得中にエラーが発生しました。管理者にお問い合わせください。");
+}
